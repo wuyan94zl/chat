f4648ede1ebf452aace8dfe488f6aa015786054d
zheng li <li.zheng@vhall.com>
2021-01-19 12:15:55 +0800

    chart-server

--- action.go
+++ action.go
@@ -1,12 +1,13 @@
 package chart
 
+// 写入聊天记录信息
 func sendMessage(userId uint64, msg Message) {
-	send := ChatMessage{}
-	send.Type = msg.Type
-	send.ChannelId = msg.ChannelId
-	send.SendUserId = userId
-	send.Content = msg.Content
-	if DB != nil{
+	if DB != nil {
+		send := ChatMessage{}
+		send.Type = msg.Type
+		send.ChannelId = msg.ChannelId
+		send.SendUserId = userId
+		send.Content = msg.Content
 		DB.Create(&send)
 	}
 }
